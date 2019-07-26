176588afc9a0aa7eaa2dd4a9e1bf8509fc782a1b
unknown <weifx@szaic.gov.cn>
2019-07-26 17:43:32 +0800

    Initial commit

---
+++ src/main/java/com/weifx/spring/erurekaclient/ErurekaclientApplication.java
@@ -0,0 +1,23 @@
+package com.weifx.spring.erurekaclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class ErurekaclientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ErurekaclientApplication.class, args);
+    }
+    @Value("${server.port}")
+    String port;
+    @RequestMapping("/getMyName")
+    public String getName(){
+        return "this is myname :"+port;
+    }
+}
